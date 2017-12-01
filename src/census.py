6b4121c90eaab39494a6ac7a03066e4af8871a8f
culk <culk@users.noreply.github.com>
2017-11-30 17:00:47 -0800

    Implemented basic sequential cnn model
    This version of the model does not work. Will have to change to
    a classification problem after doing some basic debugging. Also
    cleaned some functions by removing hard-coded numbers and removed
    imports that weren't used.

--- src/census.py
+++ src/census.py
@@ -1,27 +1,18 @@
 import os
-import random
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from shapely.wkt import loads as wkt_loads
-import shapely.wkt
-import shapely.affinity
-from shapely.geometry import MultiPolygon, Polygon
-import tifffile as tiff
 from keras.models import Model
 from keras import layers
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras import backend as K
-from sklearn.metrics import jaccard_similarity_score
 from util import load_image, load_census_mask, normalize_image, create_mask, my_jaccard
 
 '''
 Global constants
 '''
-num_class = 10
+square_km_per_pixel = 0.9
 data_dir = '/media/sf_school/project/data'
-pop_mask_folder = 'masks_census/'
 
 '''
 Preprocessing
@@ -36,8 +27,8 @@ def create_sub_image_grid(image, mask, grid_size=100):
     masks = []
     for i in range(0, H - gh, gh):
         for j in range(0, W - gw, gw):
-            sub_image = image[i:i + gh, j:j + gw, :].reshape(1, 100, 100, bands)
-            sub_mask = mask[i:i + gh, j:j + gw].reshape(1, 100, 100)
+            sub_image = image[i:i + gh, j:j + gw, :].reshape(1, gh, gw, bands)
+            sub_mask = mask[i:i + gh, j:j + gw].reshape(1, gh, gw)
             images.append(sub_image)
             masks.append(sub_mask)
     # normalize each sub image or just the entire image?
@@ -50,7 +41,7 @@ def calculate_census_weights(Y):
     return vector of weights same length as number of sub_images, each
     weight corresponds to the percent of population within that sub_image
     '''
-    weights = np.sum(Y * 0.9, axis=(1, 2))
+    weights = np.sum(Y * square_km_per_pixel, axis=(1, 2))
     weights = weights / np.sum(weights)
     return weights
 
@@ -115,22 +106,28 @@ def gen_test_dev_sets(img_type='M'):
     np.save(os.path.join(data_dir, 'dev_input_{}x{}_{}'.format(H*2, W*2, img_type)), dev_X)
     np.save(os.path.join(data_dir, 'dev_mask_{}x{}_{}'.format(H*2, W*2, num_class)), dev_Y)
 
-def create_model():
+def create_simple_cnn():
+    '''
+    Simple sequential CNN to solve the regression problem of predicting population
+    '''
     #K.image_data_format() returns channel order from config file
-    inputs = layers.Input(shape=(8, 160, 160))
-    conv1 = layers.Conv2D(32, 3, strides=1, activation='relu', padding='same', input_shape=(8, 160, 160))(inputs)
-    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+    # TODO: switch keras data format so bands are last 
+    inputs = layers.Input(shape=(64, 64, 6))
+    conv11 = layers.Conv2D(32, 3, strides=1, activation='relu', padding='same')(inputs)
+    conv12 = layers.Conv2D(32, 3, strides=1, activation='relu', padding='same')(conv11)
+    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv12)
 
-    conv2 = layers.Conv2D(64, 3, strides=1, activation='relu', padding='same')(pool1)
+    conv21 = layers.Conv2D(64, 3, strides=1, activation='relu', padding='same')(pool1)
+    conv22 = layers.Conv2D(64, 3, strides=1, activation='relu', padding='same')(conv21)
+    up1 = layers.UpSampling2D(size=(2, 2))(conv22)
 
-    #up3 = layers.merge([layers.UpSampling2D(size=(2, 2))(conv2), conv1], mode='concat', concat_axis=1)
-    up3 = layers.UpSampling2D(size=(2, 2))(conv2)
-    merge3 = layers.Concatenate(axis=1)([up3, conv1])
-    conv3 = layers.Conv2D(32, 3, strides=1, activation='relu', padding='same')(merge3)
+    conv31 = layers.Conv2D(32, 3, strides=1, activation='relu', padding='same')(up1)
+    conv32 = layers.Conv2D(32, 3, strides=1, activation='relu', padding='same')(conv31)
+    #up2 = layers.UpSampling2D(size=(2, 2))(conv32)
 
-    final = layers.Conv2D(num_class, 1, strides=1, activation='sigmoid')(conv3)
+    final = layers.Conv2D(1, 1, strides=1, activation='sigmoid')(conv32)
     model = Model(inputs=inputs, outputs=final)
-    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(), loss='mean_squared_logarithmic_error', metrics=['accuracy'])
     return model
 
 def train(model):
@@ -231,50 +228,54 @@ def predict(model, image_id):
 def main():
     # settings
     do_training = True
+    n_iters = 10
+    data_set_size = 200
+    use_prepped_data = False
     ids = ['ohio_01']
     # load images and their masks
     # TODO: save the data subset for repeated testing
-    images = []
-    masks = []
-    for i in ids:
-        image = load_image(i, 'landsat', 'all')
-        images.append(normalize_image(image))
-        mask = load_census_mask(i)
-        if mask.shape != image.shape:
-            h, w = image.shape[:2]
-            mask = mask[:h, :w]
-        masks.append(mask)
-    print(images[0].shape, np.min(images[0]), np.max(images[0]))
-    print(masks[0].shape, np.min(masks[0]), np.max(masks[0]))
-    # create X and Y composed of [#sub_images, height, width, bands]
-    X, Y = None, None
-    for image, mask in zip(images, masks):
-        sub_x, sub_y = create_sub_image_grid(image, mask)
-        if X is None or Y is None:
-            X = sub_x
-            Y = sub_y
-        else:
-            X = np.concatenate((X, sub_x))
-            Y = np.concatenate((Y, sub_y))
-    print(X.shape, np.min(X), np.max(X))
-    print(Y.shape, np.min(Y), np.max(Y))
-    # calculate weights of sub_images and randomly sample them to build train/cv set
-    weights = calculate_census_weights(Y)
-    N, *_ = weights.shape
-    selection = np.random.choice(N, size=100, replace=False, p=weights)
-    X = X[selection]
-    Y = Y[selection]
+    if use_prepped_data:
+        # load the prepped data
+        X = None
+        Y = None
+    else:
+        images = []
+        masks = []
+        for i in ids:
+            image = load_image(i, 'landsat', 'all')
+            images.append(normalize_image(image))
+            mask = load_census_mask(i)
+            if mask.shape != image.shape:
+                h, w = image.shape[:2]
+                mask = mask[:h, :w]
+            masks.append(mask)
+        print(images[0].shape, np.min(images[0]), np.max(images[0]))
+        print(masks[0].shape, np.min(masks[0]), np.max(masks[0]))
+        # create X and Y composed of [#sub_images, height, width, bands]
+        X, Y = None, None
+        for image, mask in zip(images, masks):
+            sub_x, sub_y = create_sub_image_grid(image, mask, grid_size=64)
+            if X is None or Y is None:
+                X = sub_x
+                Y = sub_y
+            else:
+                X = np.concatenate((X, sub_x))
+                Y = np.concatenate((Y, sub_y))
+        print(X.shape, np.min(X), np.max(X))
+        print(Y.shape, np.min(Y), np.max(Y))
+        # calculate weights of sub_images and randomly sample them to build train/cv set
+        weights = calculate_census_weights(Y)
+        N, *_ = weights.shape
+        selection = np.random.choice(N, size=data_set_size, replace=False, p=weights)
+        X = X[selection]
+        Y = np.expand_dims(Y[selection], axis=-1)
     # TODO: update the model to accept the new data size and to do the train/cv
-    model = create_model()
-    return
+    model = create_simple_cnn()
     if do_training:
+        print(X.shape)
+        print(Y.shape)
         model.summary()
-        train(model)
-    scores = predict(model, test_id)
-    print("Jaccard similarity scores:")
-    for cls in range(num_class):
-        print("class: {}\t score: {}".format(cls + 1, scores[cls]))
-    print("average: {}".format(sum(scores)/len(scores)))
+        model.fit(X, Y, batch_size=10, epochs=n_iters, verbose=2, validation_split=0.2)
 
 if __name__=='__main__':
     main()
